47f68f888da9549ecb93f3a92a1d6b69f5022fa8
Marcel Voss <hello@marcelvoss.com>
2014-01-28 16:50:28 +0100

    Initial commit.

---
+++ UIColor+BrandColors.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+BrandColors.h
+//  Colors
+//
+//  Created by Marcel Voß on 28/01/14.
+//  Copyright (c) 2014 Marcel Voß. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (BrandColors)
+
+@end
