c1f79e1375fd284c4d49cb3a31e86e736887b611
Marcel Voss <hello@marcelvoss.com>
2014-01-28 17:03:00 +0100

    Just added ten colors.

--- UIColor+BrandColors.h
+++ UIColor+BrandColors.h
@@ -10,4 +10,37 @@
 
 @interface UIColor (BrandColors)
 
+// 4ormat
++ (UIColor *)fourmatColor;
+
+// 5oopx
++ (UIColor *)fiveHundredPixelsColor;
+
++ (UIColor *)aboutMeBlueColor;
++ (UIColor *)aboutMeYellowColor;
++ (UIColor *)addvocateColor;
++ (UIColor *)adobeColor;
++ (UIColor *)aimColor;
++ (UIColor *)amazonColor;
++ (UIColor *)androidColor;
++ (UIColor *)angiesListColor;
++ (UIColor *)aolColor;
++ (UIColor *)asanaColor;
++ (UIColor *)atlassianColor;
++ (UIColor *)behanceColor;
++ (UIColor *)bigCartelColor;
++ (UIColor *)bitlyColor;
++ (UIColor *)bloggerColor;
++ (UIColor *)boeingColor;
++ (UIColor *)bookingDotComColor;
++ (UIColor *)carbonmadeColor;
++ (UIColor *)cheddarColor;
++ (UIColor *)cocaColaColor;
++ (UIColor *)codeSchoolColor;
++ (UIColor *)creativeMarketColor;
++ (UIColor *)deliciousColor;
++ (UIColor *)dellColor;
++ (UIColor *)designmooColor;
++ (UIColor *)deviantartColor;
+
 @end
--- UIColor+BrandColors.m
+++ UIColor+BrandColors.m
@@ -5,9 +5,73 @@
 //  Created by Marcel Voß on 28/01/14.
 //  Copyright (c) 2014 Marcel Voß. All rights reserved.
 //
+//  Color codes by http://brandcolors.net/
+//
 
 #import "UIColor+BrandColors.h"
 
 @implementation UIColor (BrandColors)
+
+
++ (UIColor *)fourmatColor
+{
+    // Hex: #fb0a2a
+    return [UIColor colorWithRed:0.984 green:0.039 blue:0.165 alpha:1.0];
+}
+
++ (UIColor *)fiveHundredPixelsColor
+{
+    // Hex: #02adea
+    return [UIColor colorWithRed:0.008 green:0.678 blue:0.918 alpha:1.0];
+}
+
++ (UIColor *)aboutMeBlueColor
+{
+    // Hex: #00405d
+    return [UIColor colorWithRed:0 green:0.251 blue:0.365 alpha:1.0];
+}
+
++ (UIColor *)aboutMeYellowColor
+{
+    // Hex: #ffcc33
+    return [UIColor colorWithRed:1 green:0.8 blue:0.2 alpha:1.0];
+}
+
++ (UIColor *)addvocateColor
+{
+    // Hex: #ff6138
+    return [UIColor colorWithRed:1 green:0.38 blue:0.22 alpha:1.0];
+}
+
++ (UIColor *)adobeColor
+{
+    // Hex: #ff0000
+    return [UIColor colorWithRed:1 green:0 blue:0 alpha:1.0];
+}
+
++ (UIColor *)aimColor
+{
+    // Hex: #fcd20b
+    return [UIColor colorWithRed:0.988 green:0.824 blue:0.043 alpha:1.0];
+}
+
++ (UIColor *)amazonColor
+{
+    // Hex: #e47911
+    return [UIColor colorWithRed:0.894 green:0.475 blue:0.067 alpha:1.0];
+}
+
++ (UIColor *)androidColor
+{
+    // Hex: #a4c639
+    return [UIColor colorWithRed:0.643 green:0.776 blue:0.224 alpha:1.0];
+}
+
++ (UIColor *)angiesListColor
+{
+    // Hex: #7fbb00
+    return [UIColor colorWithRed:0.498 green:0.733 blue:0 alpha:1.0];
+}
+
 
 @end
