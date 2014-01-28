92c995464e881776db6f2b6247e306851e877e34
Marcel Voss <hello@marcelvoss.com>
2014-01-28 17:20:15 +0100

    Added a few more colors.

--- UIColor+BrandColors.m
+++ UIColor+BrandColors.m
@@ -73,5 +73,112 @@
     return [UIColor colorWithRed:0.498 green:0.733 blue:0 alpha:1.0];
 }
 
++ (UIColor *)aolColor
+{
+    // Hex: #0060a3
+    return [UIColor colorWithRed:0 green:0.376 blue:0.639 alpha:1.0];
+}
+
++ (UIColor *)asanaColor
+{
+    // Hex: #1d8dd5
+    return [UIColor colorWithRed:0.114 green:0.553 blue:0.835 alpha:1.0];
+}
+
++ (UIColor *)atlassianColor
+{
+    // Hex: #003366
+    return [UIColor colorWithRed:0 green:0.2 blue:0.4 alpha:1.0];
+}
+
++ (UIColor *)behanceColor
+{
+    // Hex: #005cff
+    return [UIColor colorWithRed:0 green:0.361 blue:1 alpha:1.0];
+}
+
++ (UIColor *)bigCartelColor
+{
+    // Hex: #97b538
+    return [UIColor colorWithRed:0.592 green:0.71 blue:0.22 alpha:1.0];
+}
+
++ (UIColor *)bitlyColor
+{
+    // Hex: #ee6123
+    return [UIColor colorWithRed:0.933 green:0.38 blue:0.137 alpha:1.0];
+}
+
++ (UIColor *)bloggerColor
+{
+    // Hex: #fc4f08
+    return [UIColor colorWithRed:0.988 green:0.31 blue:0.031 alpha:1.0];
+}
+
++ (UIColor *)boeingColor
+{
+    // Hex: #0039a6
+    return [UIColor colorWithRed:0 green:0.224 blue:0.651 alpha:1.0];
+}
+
++ (UIColor *)bookingDotComColor
+{
+    // Hex: #003580
+    return [UIColor colorWithRed:0 green:0.208 blue:0.502 alpha:1.0];
+}
+
++ (UIColor *)carbonmadeColor
+{
+    // Hex: #613854
+    return [UIColor colorWithRed:0.38 green:0.22 blue:0.329 alpha:1.0];
+}
+
++ (UIColor *)cheddarColor
+{
+    // Hex: #ff7243
+    return [UIColor colorWithRed:1 green:0.447 blue:0.263 alpha:1.0];
+}
+
++ (UIColor *)cocaColaColor
+{
+    // Hex: #b50900
+    return [UIColor colorWithRed:0.71 green:0.035 blue:0 alpha:1.0];
+}
+
++ (UIColor *)codeSchoolColor
+{
+    // Hex: #3d4944
+    return [UIColor colorWithRed:0.239 green:0.286 blue:0.267 alpha:1.0];
+}
+
++ (UIColor *)creativeMarketColor
+{
+    // Hex: #8ba753
+    return [UIColor colorWithRed:0.545 green:0.655 blue:0.325 alpha:1.0];
+}
+
++ (UIColor *)deliciousColor
+{
+    // Hex: #205cc0
+    return [UIColor colorWithRed:0.125 green:0.361 blue:0.753 alpha:1.0];
+}
+
++ (UIColor *)dellColor
+{
+    // Hex: #3287c1
+    return [UIColor colorWithRed:0.196 green:0.529 blue:0.757 alpha:1.0];
+}
+
++ (UIColor *)designmooColor
+{
+    // Hex: #e54a4f
+    return [UIColor colorWithRed:0.898 green:0.29 blue:0.31 alpha:1.0];
+}
+
++ (UIColor *)deviantartColor
+{
+    // Hex: #4e6252
+    return [UIColor colorWithRed:0.306 green:0.384 blue:0.322 alpha:1.0];
+}
 
 @end
